ac961eae0bc80ec7605078162de8fa8c3b4afa08
yong <kyw910311@gmail.com>
2021-07-26 00:50:45 +0900

    [HACKLE-1314] remove dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,9 +9,6 @@ allprojects {
     repositories {
         mavenCentral()
         jcenter()
-        maven {
-            url = uri("https://jitpack.io")
-        }
     }
 }
 
@@ -25,7 +22,6 @@ subprojects {
     dependencies {
         implementation(kotlin("stdlib-jdk8"))
         implementation(kotlin("reflect"))
-        implementation("com.github.statsig-io:java-server-sdk:v0.3.0")
 
         testImplementation("org.junit.jupiter:junit-jupiter-api:5.5.2")
         testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.2")
