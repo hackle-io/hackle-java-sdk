860d8632ca8790fbca294708c06bf187b6c82454
yong <yong@hackle.io>
2022-03-21 12:11:26 +0900

    jackson -> gson

--- hackle-server-sdk/src/main/kotlin/io/hackle/sdk/internal/utils/Json.kt
+++ hackle-server-sdk/src/main/kotlin/io/hackle/sdk/internal/utils/Json.kt
@@ -7,7 +7,3 @@ internal val GSON: Gson = GsonBuilder().create()
 
 internal fun Any.toJson(): String = GSON.toJson(this)
 internal inline fun <reified T> String.parseJson(): T = GSON.fromJson(this, T::class.java)
-
-
-
-
