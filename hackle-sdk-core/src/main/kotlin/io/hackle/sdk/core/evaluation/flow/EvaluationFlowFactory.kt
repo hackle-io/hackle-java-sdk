9a93b4635e0dac0eefac94bd1c041bcdb083e893
reggie <jtkim@hackle.com>
2022-07-22 11:36:23 +0900

    [HACKLE-3848] 상호배타 리뷰 반영

--- hackle-sdk-core/src/main/kotlin/io/hackle/sdk/core/evaluation/flow/EvaluationFlowFactory.kt
+++ hackle-sdk-core/src/main/kotlin/io/hackle/sdk/core/evaluation/flow/EvaluationFlowFactory.kt
@@ -2,9 +2,9 @@ package io.hackle.sdk.core.evaluation.flow
 
 import io.hackle.sdk.core.evaluation.action.ActionResolver
 import io.hackle.sdk.core.evaluation.bucket.Bucketer
+import io.hackle.sdk.core.evaluation.container.ContainerResolver
 import io.hackle.sdk.core.evaluation.match.ConditionMatcherFactory
 import io.hackle.sdk.core.evaluation.match.TargetMatcher
-import io.hackle.sdk.core.evaluation.container.ContainerResolver
 import io.hackle.sdk.core.evaluation.target.ExperimentTargetDeterminer
 import io.hackle.sdk.core.evaluation.target.OverrideResolver
 import io.hackle.sdk.core.evaluation.target.TargetRuleDeterminer
@@ -29,10 +29,11 @@ internal class EvaluationFlowFactory {
 
     init {
 
+        val bucketer = Bucketer()
         val targetMatcher = TargetMatcher(ConditionMatcherFactory())
-        val actionResolver = ActionResolver(Bucketer())
+        val actionResolver = ActionResolver(bucketer)
         val overrideResolver = OverrideResolver(targetMatcher, actionResolver)
-        val containerResolver = ContainerResolver(Bucketer())
+        val containerResolver = ContainerResolver(bucketer)
 
         val abTestFlow = EvaluationFlow.of(
             OverrideEvaluator(overrideResolver),
