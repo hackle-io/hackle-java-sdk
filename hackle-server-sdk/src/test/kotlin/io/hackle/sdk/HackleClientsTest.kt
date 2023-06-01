0cd5e90b2608d3aa4ca7bf3a4298696b349340d6
yong <yong@hackle.io>
2023-06-01 18:29:58 +0900

    singletone client per sdk key

--- hackle-server-sdk/src/test/kotlin/io/hackle/sdk/HackleClientsTest.kt
+++ hackle-server-sdk/src/test/kotlin/io/hackle/sdk/HackleClientsTest.kt
@@ -3,9 +3,38 @@ package io.hackle.sdk
 import io.hackle.sdk.internal.client.HackleClientImpl
 import org.junit.jupiter.api.Test
 import strikt.api.expectThat
+import strikt.assertions.hasSize
 import strikt.assertions.isA
+import strikt.assertions.isNotEqualTo
+import java.util.concurrent.Callable
+import java.util.concurrent.CyclicBarrier
+import java.util.concurrent.Executors
 
 internal class HackleClientsTest {
+
+    @Test
+    fun `create concurrency`() {
+        val config = HackleConfig.builder()
+            .sdkUrl("localhost")
+            .eventUrl("localhost")
+            .monitoringUrl("localhost")
+            .build()
+
+        val barrier = CyclicBarrier(100)
+        val executor = Executors.newFixedThreadPool(100)
+
+        val futures = List(100) {
+            executor.submit(Callable {
+                barrier.await()
+                HackleClients.create("SDK_KEY", config)
+            })
+        }
+        val clients = futures.map { it.get() }
+        expectThat(clients.distinct()).hasSize(1)
+
+        val client = HackleClients.create("SDK2", config)
+        expectThat(clients[0]).isNotEqualTo(client)
+    }
 
     @Test
     fun `create`() {
