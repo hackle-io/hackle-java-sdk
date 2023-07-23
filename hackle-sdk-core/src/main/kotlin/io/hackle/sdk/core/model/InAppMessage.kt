ced73b4a3ce759e25f252aeca6daf1e5088eaf5e
yong <yong@hackle.io>
2023-07-23 20:10:01 +0900

    refactor iam

--- hackle-sdk-core/src/main/kotlin/io/hackle/sdk/core/model/InAppMessage.kt
+++ hackle-sdk-core/src/main/kotlin/io/hackle/sdk/core/model/InAppMessage.kt
@@ -1,7 +1,5 @@
 package io.hackle.sdk.core.model
 
-import java.io.Serializable
-
 data class InAppMessage(
     val id: Long,
     val key: Long,
@@ -10,7 +8,7 @@ data class InAppMessage(
     val eventTrigger: EventTrigger,
     val targetContext: TargetContext,
     val messageContext: MessageContext
-) : Serializable {
+) {
 
     enum class Status {
         INITIALIZED,
@@ -55,7 +53,7 @@ data class InAppMessage(
         X_BUTTON
     }
 
-    sealed class Period : Serializable {
+    sealed class Period {
         fun within(timestamp: Long): Boolean {
             return when (this) {
                 is Always -> true
@@ -74,44 +72,44 @@ data class InAppMessage(
     data class EventTrigger(
         val rules: List<Rule>,
         val frequencyCap: FrequencyCap?
-    ) : Serializable {
+    ) {
         data class Rule(
             val eventKey: String,
             val targets: List<Target>
-        ) : Serializable
+        )
 
         data class FrequencyCap(
             val identifierCaps: List<IdentifierCap>,
             val durationCap: DurationCap?
-        ) : Serializable
+        )
 
         data class IdentifierCap(
             val identifierType: String,
             val count: Int
-        ) : Serializable
+        )
 
         data class DurationCap(
             val durationMillis: Long,
             val count: Int
-        ) : Serializable
+        )
     }
 
     data class TargetContext(
         val targets: List<Target>,
         val overrides: List<UserOverride>
-    ) : Serializable
+    )
 
     data class UserOverride(
         val identifierType: String,
         val identifiers: List<String>
-    ) : Serializable
+    )
 
     data class MessageContext(
         val defaultLang: String,
         val platformTypes: List<PlatformType>,
         val orientations: List<Orientation>,
         val messages: List<Message>
-    ) : Serializable
+    )
 
     data class Message(
         val lang: String,
@@ -121,56 +119,56 @@ data class InAppMessage(
         val buttons: List<Button>,
         val closeButton: Button?,
         val background: Background
-    ) : Serializable {
+    ) {
 
         data class Layout(
             val displayType: DisplayType,
             val layoutType: LayoutType
-        ) : Serializable
+        )
 
         data class Image(
             val orientation: Orientation,
             val imagePath: String,
             val action: Action?
-        ) : Serializable
+        )
 
         data class Text(
             val title: Attribute,
             val body: Attribute
-        ) : Serializable {
+        ) {
             data class Attribute(
                 val text: String,
                 val style: Style
-            ) : Serializable
+            )
 
             data class Style(
                 val textColor: String
-            ) : Serializable
+            )
         }
 
         data class Button(
             val text: String,
             val style: Style,
             val action: Action
-        ) : Serializable {
+        ) {
 
             data class Style(
                 val textColor: String,
                 val bgColor: String,
                 val borderColor: String
-            ) : Serializable
+            )
         }
 
         data class Background(
             val color: String
-        ) : Serializable
+        )
     }
 
     data class Action(
         val behavior: Behavior,
         val type: ActionType,
         val value: String?
-    ) : Serializable
+    )
 }
 
 internal fun InAppMessage.supports(platform: InAppMessage.PlatformType): Boolean {
