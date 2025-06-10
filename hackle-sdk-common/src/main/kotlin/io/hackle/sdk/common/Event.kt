3b9230db2d06e7c57365839f3bb0ddfc8dcd9aa6
sungwoo-hackle <sungwoo.yeo@hackle.io>
2025-06-10 17:48:38 +0900

    add HackleCommonEventBuilder

--- hackle-sdk-common/src/main/kotlin/io/hackle/sdk/common/Event.kt
+++ hackle-sdk-common/src/main/kotlin/io/hackle/sdk/common/Event.kt
@@ -7,16 +7,11 @@ data class Event internal constructor(
     override val key: String,
     override val value: Double?,
     override val properties: Map<String, Any>
-): HackleCommonEvent {
+) : HackleCommonEvent {
     override val internalProperties: Map<String, Any>? = null
 
-    class Builder(private val key: String) {
-        private var value: Double? = null
-        private val properties = PropertiesBuilder()
-        fun value(value: Double) = apply { this.value = value }
-        fun property(key: String, value: Any?) = apply { this.properties.add(key, value) }
-        fun properties(properties: Map<String, Any?>?) = apply { properties?.let { this.properties.add(it) } }
-        fun build() = Event(key, value, properties.build())
+    class Builder(key: String) : HackleCommonEvent.HackleCommonEventBuilder<Event>(key) {
+        override fun build() = Event(key, value, properties.build())
     }
 
     companion object {
--- hackle-sdk-common/src/main/kotlin/io/hackle/sdk/common/HackleCommonEvent.kt
+++ hackle-sdk-common/src/main/kotlin/io/hackle/sdk/common/HackleCommonEvent.kt
@@ -5,4 +5,16 @@ interface HackleCommonEvent {
     val value: Double?
     val properties: Map<String, Any>
     val internalProperties: Map<String, Any>?
+
+    abstract class HackleCommonEventBuilder<T : HackleCommonEvent>(protected val key: String) {
+        protected var value: Double? = null
+        protected val properties = PropertiesBuilder()
+        protected var internalProperties: PropertiesBuilder? = null
+
+        fun value(value: Double) = apply { this.value = value }
+        fun property(key: String, value: Any?) = apply { this.properties.add(key, value) }
+        fun properties(properties: Map<String, Any?>?) = apply { properties?.let { this.properties.add(it) } }
+
+        abstract fun build(): T
+    }
 }
