04343d3fd5d55809ef5784260b44384c59915add
sungwoo-hackle <sungwoo.yeo@hackle.io>
2025-07-01 16:36:02 +0900

    update publish url

--- build.gradle.kts
+++ build.gradle.kts
@@ -80,6 +80,8 @@ subprojects {
 nexusPublishing {
     repositories {
         sonatype {
+            nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
             username.set(System.getenv("SONATYPE_USERNAME"))
             password.set(System.getenv("SONATYPE_PASSWORD"))
         }
